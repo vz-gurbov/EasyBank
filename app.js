dc8abb89a902b4d056eb9e0dbc38742aa52265e3
vz-gurbov <vz.gurbov@abv.bg>
2020-08-19 18:18:14 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+function toggle() {
+    var blur = document.getElementById('modal');
+    blur.classList.toggle('active');
+
+    var blur = document.getElementById('popup');
+    blur.classList.toggle('active');
+}
